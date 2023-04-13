ab5cdf6c686f0fb647a1e14ea053eaa4a8608f23
Mateusz Gurdała <mateusz.gurdala.mg@gmail.com>
2023-04-13 15:14:47 +0200

    Created app base component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, Renderer2 } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { GameBoardComponent } from "./game-board/game-board.component";
+import { BoardSegmentComponent } from "./board-segment/board-segment.component";
+import { CreateBoardDirective } from './create-board.directive';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, GameBoardComponent, BoardSegmentComponent, CreateBoardDirective],
+	imports: [BrowserModule, AppRoutingModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
