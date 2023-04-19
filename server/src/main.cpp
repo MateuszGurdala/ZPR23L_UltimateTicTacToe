b58c406f55bafb6f9850f05dca62fa44b8883913
Mateusz Gurdała <mateusz.gurdala.mg@gmail.com>
2023-04-19 17:49:13 +0200

    Code improvements

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,49 +1,31 @@
-﻿#include "../include/HttpRequest.hpp"
-#include "../include/HttpResponse.hpp"
-#include <cstdio>
-#include <cstdlib>
-#include <memory>
-
-#ifdef _WIN32
-#include "../include/WIN32ServerSocket.hpp"
-#endif
-
-#if defined(__linux__) || defined(__arm64__)
-#include "../include/LinuxServerSocket.hpp"
-#endif
-
-#define DEFAULT_PORT 1337
-#define DEFAULT_REQUEST_BUFFLEN 1024
-
-typedef std::unique_ptr<HttpRequest> URequest;
-typedef std::unique_ptr<HttpResponse> UResponse;
-
-bool verbose = true;
+﻿#include "main.hpp"
+#include "config.hpp"
 
 int main() {
+  using URequest = std::shared_ptr<HttpRequest>;
+
 #ifdef _WIN32
   WIN32ServerSocket serverSocket(DEFAULT_PORT, DEFAULT_REQUEST_BUFFLEN);
 #endif
 
 #if defined(__linux__) || defined(__arm64__)
-  LinuxServerSocket serverSocket(DEFAULT_PORT, DEFAULT_REQUEST_BUFFLEN);
+  LinuxServerSocket serverSocket(app::port, app::requestBuffer);
 #endif
 
+  const HttpHandler handler;
   URequest request;
-  UResponse();
 
   if (serverSocket.Init() != 0) {
-    printf("Socket intialization failed.\n");
+    std::cout << "Socket intialization failed.\n";
     return 1;
   }
 
   do {
     if (serverSocket.Listen() == 0) {
-      request = std::make_unique<HttpRequest>(serverSocket.GetRequest());
+      request = std::make_shared<HttpRequest>(serverSocket.GetRequest());
 
-      // TODO: REMOVE
-      HttpResponse response("{\"result\":\"true\"}");
-      serverSocket.SendResponse(response);
+      auto x = handler.handle(request);
+      serverSocket.SendResponse(x);
     }
   } while (true);
 
