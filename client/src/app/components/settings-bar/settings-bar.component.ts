591496ecf5701069585c4b16601e9c1bb9cd50d1
Mateusz Gurdała <mateusz.gurdala.mg@gmail.com>
2023-04-20 23:50:04 +0200

    Added basic settings bar

--- client/src/app/components/settings-bar/settings-bar.component.ts
+++ client/src/app/components/settings-bar/settings-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { GameMasterService } from "src/app/services/game-master.service";
 
 @Component({
 	selector: "settings-bar",
@@ -6,7 +7,11 @@ import { Component } from "@angular/core";
 	styleUrls: ["./settings-bar.component.css"],
 })
 export class SettingsBarComponent {
-	isExtended: boolean = false;
+	isExtended: boolean = true;
+	boardSize: string = "3";
+	serverUrl: string = "localhost:4200";
+
+	constructor(private master: GameMasterService) {}
 
 	extend() {
 		this.isExtended = !this.isExtended;
