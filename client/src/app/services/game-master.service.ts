fbd9eb57a0024729c6081c9b1d166c74bd3dde22
Mateusz Gurdała <mateusz.gurdala.mg@gmail.com>
2023-04-21 14:57:10 +0200

    Board can now be updated based on data from server

--- client/src/app/services/game-master.service.ts
+++ client/src/app/services/game-master.service.ts
@@ -1,19 +1,21 @@
 import { Injectable } from "@angular/core";
 import { GameHttpClient } from "./game-http-client.service";
-
-/*
-GetGameState
-GetBoardState
-MakeMove
-CreateBoard
-EndGame
-*/
+import { GameBoardComponent } from "../components/game-board/game-board.component";
 
 @Injectable({
 	providedIn: "root",
 })
 export class GameMasterService {
 	constructor(private httpClient: GameHttpClient) {}
+	gameBoard: GameBoardComponent;
+
+	setBoardComponent(component: GameBoardComponent): void {
+		this.gameBoard = component;
+	}
+
+	setServerUrl(url: string): void {
+		this.httpClient.url = "http://" + url;
+	}
 
 	makeMove(boardId: number, segmentId: number): void {
 		this.httpClient.postMove(boardId, segmentId).subscribe((next) => {
@@ -21,5 +23,9 @@ export class GameMasterService {
 		});
 	}
 
-	getGameState(): void {}
+	updateBoard(): void {
+		this.httpClient.getBoardState().subscribe((next) => {
+			this.gameBoard.update(next);
+		});
+	}
 }
