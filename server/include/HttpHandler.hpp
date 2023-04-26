3e2f4e3201979da8b5c08480515461564beb020f
Mateusz Gurdała <mateusz.gurdala.mg@gmail.com>
2023-04-27 01:14:49 +0200

    Added documentation

--- server/include/HttpHandler.hpp
+++ server/include/HttpHandler.hpp
@@ -5,17 +5,89 @@
 
 #include "HttpRequest.hpp"
 #include "HttpResponse.hpp"
+/*  Class: HttpHandler
+    A singleton class that receives and processes HttpRequest objects.
+    Runs GameHandler methods based on request method and endpoint.
+    Creates HttpResponse objects and passes them back to the socket.
 
+*/
 class HttpHandler {
 private:
+  /*  Function: handleGETRequest
+
+      Processes requests with GET method.
+
+      Parameters:
+
+        request - HttpRequest object for processing.
+
+      Returns:
+
+        HttpResponse object with/without body and status code 200 or empty
+        response with status code 400, based on if GameHandler method ended with
+        success.
+
+      See Also:
+
+        <HttpRequest>
+  */
   HttpResponse
   handleGETRequest(const std::shared_ptr<HttpRequest> request) const;
+  /*  Function: handlePOSTRequest
+
+    Processes requests with POST method.
+
+    Parameters:
+
+      request - HttpRequest object for processing.
+
+    Returns:
+
+      HttpResponse object with status code 200 or 400, based on if GameHandler
+      method ended with success.
+
+    See Also:
+
+      <HttpRequest>
+*/
   HttpResponse
   handlePOSTRequest(const std::shared_ptr<HttpRequest> request) const;
+  /*  Function: handleOPTIONSRequest
+
+    Processes requests with OPTIONS method.
+
+    Parameters:
+
+      request - HttpRequest object for processing.
+
+    Returns:
+
+      HttpResponse object with Accept Headers.
+
+    See Also:
+
+      <HttpRequest>
+*/
   HttpResponse
   handleOPTIONSRequest(const std::shared_ptr<HttpRequest> request) const;
 
 public:
+  /*  Function: handle
+
+    Processes HttpRequest objects.
+
+    Parameters:
+
+      request - HttpRequest object for processing.
+
+    Returns:
+
+      HttpResponse object.
+
+    See Also:
+
+      <HttpRequest>
+*/
   HttpResponse handle(const std::shared_ptr<HttpRequest> request) const;
 };
 
--- server/include/HttpHeaders.hpp
+++ server/include/HttpHeaders.hpp
@@ -17,6 +17,10 @@ const std::map<std::string, std::string> globalHeaders = {
     {"Allow", config::allowMethods},
     {"Access-Control-Allow-Headers", config::allowHeaders}};
 
+/*  Class: HttpHeaders
+    Helper class used for parsing and accessing http headers, or for building
+    headers based on request requirements.
+*/
 class HttpHeaders {
 private:
   const std::string _newLine = "\n";
@@ -25,21 +29,150 @@ private:
   std::map<std::string, std::string> _headers;
 
 public:
+  /*  Constructor: HttpHeaders
+
+          Default constructor. Used to add headers to HttpResponse object.
+
+        See Also:
+
+          <HttpResponse>
+    */
   HttpHeaders();
+  /*  Constructor: HttpHeaders
+
+        Constructor that parses headers from string object. Used when generating
+        HttpRequest object.
+
+      Parameters:
+
+        headers - reference to std::string that contains raw headers.
+
+      See Also:
+
+        <HttpRequest>
+*/
   explicit HttpHeaders(std::string &headers);
+  /*  Function: addHeader
 
+    Adds key-value pair as header data to internal std::map structure.
+
+    Parameters:
+
+      key - Header name.
+      value - Header value.
+
+    Returns:
+
+      1 if header with same name was already added. Else 0.
+*/
   int addHeader(std::string &key, const std::string &value);
-  int addHeader(std::string &&key, const std::string &value);
-  int addHeader(std::string &&key, std::string &&value);
-  int addDateHeader();
-  int addAllowHeader();
-  int addContentLengthHeader(int length);
-  int closeConnection();
-  int addCORSHeaders();
-  std::string toString() const;
+  /*  Function: addHeader
 
+        Adds key-value pair as header data to internal std::map structure.
+
+      Parameters:
+
+        key - Header name.
+        value - Header value.
+
+      Returns:
+
+        1 if header with same name was already added. Else 0.
+*/
+  int addHeader(std::string &&key, const std::string &value);
+  /*  Function: addHeader
+
+        Adds key-value pair as header data to internal std::map structure.
+
+      Parameters:
+
+        key - Header name.
+        value - Header value.
+
+      Returns:
+
+        1 if header with same name was already added. Else 0.
+*/
+  int addHeader(std::string &&key, std::string &&value);
+  /*  Function: addDateHeader
+
+        Adds a header with the date and time the response was generated.
+
+      Returns:
+
+        1 if header with same name was already added. Else 0.
+*/
+  int addDateHeader();
+  /*  Function: addAllowHeader
+
+        Adds a header with data types that are accepted by the server.
+
+      Returns:
+
+        1 if header with same name was already added. Else 0.
+*/
+  int addAllowHeader();
+  /*  Function: addContentLengthHeader
+
+        Adds a header with content length.
+
+      Parameters:
+
+        length - Length of response body.
+
+      Returns:
+
+        1 if header with same name was already added. Else 0.
+*/
+  int addContentLengthHeader(int length);
+  /*  Function: closeConnection
+
+        Adds a header that indicates that connection should be closed.
+
+      Returns:
+
+      1 if header with same name was already added. Else 0.
+*/
+  int closeConnection();
+  /*  Function: addCORSHeaders
+
+        Adds a header that indicates that connection should be closed.
+
+      Returns:
+
+        1 if header with same name was already added. Else 0.
+*/
+  int addCORSHeaders();
+  /*  Function: toString
+
+      Returns:
+
+        All added headers as std::string formatted to be understood by web
+        browsers.
+*/
+  std::string toString() const;
+  /*  Function: verbose
+
+        Prints header data to standard output.
+*/
   void verbose() const;
+  /*  Function: operator[]
+
+        Operator overload used to more easily obtain header data by its name.
+
+      Parameters:
+
+        key - Header name.
+
+      Returns:
+
+        Header data.
+*/
   const std::string &operator[](const std::string &key) const;
+  /*  Function: operator=
+
+        Copy operator.
+*/
   HttpHeaders &operator=(const HttpHeaders &obj);
 };
 
