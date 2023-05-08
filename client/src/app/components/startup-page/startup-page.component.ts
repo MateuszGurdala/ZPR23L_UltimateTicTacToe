9fd00d0f34236bc77208b444289fb8a156f31359
Mateusz Gurdała <mateusz.gurdala.mg@gmail.com>
2023-05-08 22:20:38 +0200

    Started creating startup page

--- client/src/app/components/startup-page/startup-page.component.ts
+++ client/src/app/components/startup-page/startup-page.component.ts
@@ -1,8 +1,21 @@
 import { Component } from "@angular/core";
-
+import { GameMasterService } from "../../services/game-master.service";
 @Component({
 	selector: "startup-page",
 	templateUrl: "./startup-page.component.html",
 	styleUrls: ["./startup-page.component.css"],
 })
-export class StartupPageComponent {}
+export class StartupPageComponent {
+	displayWelcome: boolean = false;
+	displayOptions: boolean = false;
+
+	constructor(private master: GameMasterService) {}
+
+	returnClick(): void {
+		this.displayWelcome = true;
+	}
+
+	optionsClick(): void {
+		this.displayWelcome = false;
+	}
+}
