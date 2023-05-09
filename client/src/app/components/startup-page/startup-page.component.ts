edb88707885ccb01ace9d09262c44d81971b20ba
Mateusz Gurdała <mateusz.gurdala.mg@gmail.com>
2023-05-09 15:37:04 +0200

    Further developed settings page

--- client/src/app/components/startup-page/startup-page.component.ts
+++ client/src/app/components/startup-page/startup-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, Input } from "@angular/core";
 import { GameMasterService } from "../../services/game-master.service";
 @Component({
 	selector: "startup-page",
@@ -7,15 +7,25 @@ import { GameMasterService } from "../../services/game-master.service";
 })
 export class StartupPageComponent {
 	displayWelcome: boolean = false;
-	displayOptions: boolean = false;
+	displayOptions: boolean = true;
+	preferredSign: "X" | "O" = "X";
+	xIsChosen: boolean = true;
 
 	constructor(private master: GameMasterService) {}
 
 	returnClick(): void {
 		this.displayWelcome = true;
+		this.displayOptions = false;
 	}
 
 	optionsClick(): void {
 		this.displayWelcome = false;
+		this.displayOptions = true;
+	}
+
+	selectSign(arg: PointerEvent | MouseEvent): void {
+		this.preferredSign = (arg.target as HTMLElement).innerHTML as "X" | "O";
+
+		this.xIsChosen = this.preferredSign == "X";
 	}
 }
