c69551df126255547c787b7dbeeab3ac0ddd2540
Mateusz Gurdała <mateusz.gurdala.mg@gmail.com>
2023-06-05 17:56:54 +0200

    Refactored code and added missing documentation

--- server/include/http/HttpHandler.hpp
+++ server/include/http/HttpHandler.hpp
@@ -15,6 +15,12 @@
 */
 class HttpHandler {
 private:
+  unsigned int boardSize;
+  bool isPlayerVsComputer = true;
+  bool isGuestConnected = false;
+  char hostSign;
+  char guestSign;
+  std::unique_ptr<GameHandler> gameHandler;
   /*  Function: handleGETRequest
 
     Processes requests with GET method.
@@ -34,25 +40,6 @@ private:
       <HttpRequest>
 */
   HttpResponse handleGETRequest(const std::shared_ptr<HttpRequest> &request);
-  //   /*  Function: handlePOSTRequest
-
-  //     Processes requests with POST method.
-
-  //     Parameters:
-
-  //       request - HttpRequest object for processing.
-
-  //     Returns:
-
-  //       HttpResponse object with status code 200 or 400, based on if
-  //       GameHandler method ended with success.
-
-  //     See Also:
-
-  //       <HttpRequest>
-  // */
-  //   HttpResponse
-  //   handlePOSTRequest(const std::shared_ptr<HttpRequest>& request) const;
   /*  Function: handleOPTIONSRequest
 
     Processes requests with OPTIONS method.
@@ -66,19 +53,167 @@ private:
       <HttpRequest>
 */
   HttpResponse handleOPTIONSRequest() const;
+  /*  Function: verifyPlayer
 
-  // TODO: Documentation
-  unsigned int boardSize;
-  bool isPlayerVsComputer = true;
-  bool isGuestConnected = false;
-  char hostSign;
-  char guestSign;
+    Authorizes request sender based on cookie value.
 
-  std::unique_ptr<GameHandler> gameHandler;
+    Returns:
+
+      True if player is verified, otherwise false.
+
+    See Also:
+
+      <HttpRequest>
+      <extractCookieValue>
+*/
   bool verifyPlayer(const std::shared_ptr<HttpRequest> &request) const;
+  /*  Function: extractCookieValue
+
+    Extracts player cookie from the request headers and compares its value to
+    accepted values. If cookie with the name "player" is absent or contains
+    an incorrect value, an error will be thrown.
+
+    Returns:
+
+      Cookie value as std::string.
+
+    See Also:
+
+      <HttpRequest>
+      <verifyPlayer>
+*/
   std::string
   extractCookieValue(const std::shared_ptr<HttpRequest> &request) const;
+  /*  Function: serverState
+
+    Returns:
+
+      Current server state as HttpResponse object.
+
+    See Also:
+
+      <HttpRequest>
+      <HttpResponse>
+      <serverStateEndpoint>
+*/
   HttpResponse serverState(const std::shared_ptr<HttpRequest> &request) const;
+  /*  Function: boardStateEndpoint
+
+    If game is initialised, it returns the current state of the board in json
+    format.
+
+    Returns:
+
+      HttpResponse object with a GameHandler::BoardStateAsJson() return value;
+
+    See Also:
+
+      <HttpRequest>
+      <BoardStateAsJson>
+*/
+  HttpResponse boardStateEndpoint() const;
+  /*  Function: serverStateEndpoint
+
+    Returns:
+
+      HttpResponse object with Accept Headers.
+
+    See Also:
+
+      <HttpRequest>
+      <serverState>
+*/
+  HttpResponse
+  serverStateEndpoint(const std::shared_ptr<HttpRequest> &request) const;
+  /*  Function: gameStageEndpoint
+
+    Returns:
+
+      Current game stage as HttpResponse object if game is initialized.
+
+    See Also:
+
+      <HttpRequest>
+      <HttpResponse>
+*/
+  HttpResponse gameStageEndpoint() const;
+  /*  Function: currentSegmentEndpoint
+
+    Returns:
+
+      Currently active segment number as HttpResponse object. If value is
+    unknown, it returns -1;
+
+    See Also:
+
+      <HttpRequest>
+      <GameHandler>
+      <GetSegmentIndex>
+*/
+  HttpResponse currentSegmentEndpoint() const;
+  /*  Function: endGameEndpoint
+
+    Ends game on behalf on sender. Removes player cookie.
+
+    Returns:
+
+      HttpResponse object with boolean value that indicates if operation ended
+    in success.
+
+    See Also:
+
+      <HttpRequest>
+*/
+  HttpResponse endGameEndpoint(const std::shared_ptr<HttpRequest> &request);
+  /*  Function: makeMoveEndpoint
+
+    Makes move based on given board and segment values. If sender is not
+    authorized or move is illegal, it returns invalid request HttpResponse
+    object.
+
+    Returns:
+
+      GameHandler::MoveAsJson as body of HttpResponse object.
+
+    See Also:
+
+      <HttpRequest>
+      <GameHandler>
+      <PerformTurn>
+      <MoveAsJson>
+*/
+  HttpResponse makeMoveEndpoint(const std::shared_ptr<HttpRequest> &request);
+  /*  Function: createGameEndpoint
+
+    Creates GameHandler object on server with board size, game mode and host
+    sign values from request.
+
+    Returns:
+
+      HttpResponse object with player Set-Cookie header.
+
+    See Also:
+
+      <HttpRequest>
+      <GameHandler>
+*/
+  HttpResponse createGameEndpoint(const std::shared_ptr<HttpRequest> &request);
+  /*  Function: joinGameEndpoint
+
+    Processes requests with OPTIONS method.
+
+    Returns:
+
+      HttpResponse object with a Boolean value indicating whether the player has
+      successfully joined the game. If true, the player's cookie is set in the
+      response.
+
+    See Also:
+
+      <HttpRequest>
+      <HttpResponse>
+*/
+  HttpResponse joinGameEndpoint(const std::shared_ptr<HttpRequest> &request);
 
 public:
   /*  Function: handle
--- server/lib/httphandler/HttpHandler.cpp
+++ server/lib/httphandler/HttpHandler.cpp
@@ -23,156 +23,22 @@ HttpHandler::handleGETRequest(const std::shared_ptr<HttpRequest> &request) {
 
   const std::string endpoint = request->getEndpoint();
 
-  /* Board State */
   if (endpoint == "/BoardState") {
-    if (gameHandler) {
-      return HttpResponse::GETResponse(gameHandler->BoardStateAsJson());
-    }
-    return HttpResponse::ERRORResponse("400", "Game not yet created");
-  }
-  /* Game State */
-  else if (endpoint == "/ServerState") {
-    return serverState(request);
-  }
-  /* Game Stage */
-  else if (endpoint == "/GameStage") {
-    /*
-        "Player X Turn",
-        "Player O Turn",
-        "Game is Finished",
-        "Player X Turn, choose segment",
-        "Player O Turn, choose segment",
-        "Unknown",
-    */
-    if (gameHandler) {
-      return HttpResponse::GETResponse(gameHandler->GetGameStage());
-    }
-    return HttpResponse::ERRORResponse("501", "NOT IMPLEMENTED");
-  }
-  /* Server Status */
-  else if (endpoint == "/ServerStatus") {
-    return HttpResponse::GETResponse(R"(true)");
-  }
-  /* End Game */
-  else if (endpoint == "/EndGame") {
-    if (verifyPlayer(request)) {
-      std::string player = extractCookieValue(request);
-      auto response = HttpResponse::GETResponse(R"(true)");
-
-      gameHandler.reset();
-      isGuestConnected = false;
-
-      if (player.compare("playerX")) {
-        response.removeCookie("player", "playerX");
-      } else if (player.compare("playerO")) {
-        response.removeCookie("player", "playerO");
-      }
-
-      return response;
-    } else {
-      return HttpResponse::ERRORResponse("401", "UNAUTHORIZED");
-    }
-  }
-  /* Make Move */
-  else if (endpoint == "/MakeMove") {
-    if (verifyPlayer(request) && gameHandler) {
-      std::string player = extractCookieValue(request);
-      std::string boardId = (request->queryParams)["boardId"];
-      std::string segmentId = (request->queryParams)["segmentId"];
-
-      unsigned int boardIdIndex = std::stoul(boardId);
-      unsigned int segmentIdIndex = std::stoul(segmentId);
-
-      Point outerCoordinates =
-          BoardIndexConverter::IndexToPoint(boardIdIndex, boardSize);
-      Point innerCoordinates =
-          BoardIndexConverter::IndexToPoint(segmentIdIndex, boardSize);
-      std::array<Point, 2> move = {outerCoordinates, innerCoordinates};
-
-      bool isValid = gameHandler->PerformMoveValidation(outerCoordinates,
-                                                        innerCoordinates);
-      if (isValid) {
-        gameHandler->PerformTurn(outerCoordinates, innerCoordinates);
-      }
-      return HttpResponse::GETResponse(
-          gameHandler->MoveAsJson(false, move, isValid));
-    } else {
-      return HttpResponse::ERRORResponse("401", "UNAUTHORIZED");
-    }
-  }
-  /* Create Game */
-  else if (endpoint == "/CreateGame") {
-    if (verifyPlayer(request)) {
-      return HttpResponse::GETResponse(R"(false)");
-    } else {
-      std::string playerType;
-      std::string gameMode = (request->queryParams)["gameMode"];
-      std::string playerSign = (request->queryParams)["playerSign"];
-      std::string boardSizeQuery = (request->queryParams)["boardSize"];
-      char playerSignAsChar = playerSign.c_str()[0];
-
-      if (playerSignAsChar != 'X' && playerSignAsChar != 'O') {
-        return HttpResponse::ERRORResponse("400",
-                                           "Invalid player "
-                                           "figure while creating game.");
-      }
-
-      isPlayerVsComputer = gameMode == "Player_vs_AI" ? true : false;
-
-      boardSize = std::stoi(boardSizeQuery);
-      gameHandler = std::make_unique<GameHandler>(
-          std::stoi(boardSizeQuery), playerSignAsChar, isPlayerVsComputer);
-
-      // Assume game has been successfully created
-      auto response = HttpResponse::GETResponse(R"(true)");
-
-      if (playerSign == "X") {
-        hostSign = 'X';
-        guestSign = 'O';
-        if (!isPlayerVsComputer) {
-          response.setCookie("player", "playerX");
-        }
-      } else {
-        hostSign = 'O';
-        guestSign = 'X';
-        if (!isPlayerVsComputer) {
-          response.setCookie("player", "playerO");
-        }
-      }
-
-      if (isPlayerVsComputer) {
-        response.setCookie("player", "playerSolo");
-      }
-
-      return response;
-    }
-  }
-  /* Join Game */
-  else if (endpoint == "/JoinGame") {
-    auto falseResponse = HttpResponse::GETResponse(R"(false)");
-    if (verifyPlayer(request) || gameHandler.get() == NULL) {
-    } else if (gameHandler.get() != NULL && isPlayerVsComputer) {
-    } else if (gameHandler.get() != NULL && !isPlayerVsComputer) {
-      if (isGuestConnected) {
-      } else {
-        isGuestConnected = true;
-        if (guestSign == 'X') {
-          auto response = HttpResponse::GETResponse(R"("X")");
-          response.setCookie("player", "playerX");
-          return response;
-        } else {
-          auto response = HttpResponse::GETResponse(R"("O")");
-          response.setCookie("player", "playerO");
-          return response;
-        }
-      }
-    }
-    return falseResponse;
-  }
-  /* Current Segment */
-  else if (endpoint == "/CurrentSegment") {
-    auto segment = gameHandler->GetSegmentIndex();
-    return HttpResponse::GETResponse(std::to_string(segment));
+    return boardStateEndpoint();
+  } else if (endpoint == "/ServerState") {
+    return serverStateEndpoint(request);
+  } else if (endpoint == "/GameStage") {
+    return gameStageEndpoint();
+  } else if (endpoint == "/EndGame") {
+    return endGameEndpoint(request);
+  } else if (endpoint == "/MakeMove") {
+    return makeMoveEndpoint(request);
+  } else if (endpoint == "/CreateGame") {
+    return createGameEndpoint(request);
+  } else if (endpoint == "/JoinGame") {
+    return joinGameEndpoint(request);
+  } else if (endpoint == "/CurrentSegment") {
+    return currentSegmentEndpoint();
   }
   /* Invalid Endpoint */
   else {
@@ -271,4 +137,147 @@ HttpHandler::serverState(const std::shared_ptr<HttpRequest> &request) const {
   }
 
   return HttpResponse::GETResponse(stateStr);
+}
+
+HttpResponse HttpHandler::boardStateEndpoint() const {
+  if (gameHandler) {
+    return HttpResponse::GETResponse(gameHandler->BoardStateAsJson());
+  }
+  return HttpResponse::ERRORResponse("400", "Game not yet created");
+}
+
+HttpResponse HttpHandler::serverStateEndpoint(
+    const std::shared_ptr<HttpRequest> &request) const {
+  return serverState(request);
+}
+
+HttpResponse HttpHandler::gameStageEndpoint() const {
+  if (gameHandler) {
+    return HttpResponse::GETResponse(gameHandler->GetGameStage());
+  }
+  return HttpResponse::ERRORResponse("501", "NOT IMPLEMENTED");
+}
+
+HttpResponse
+HttpHandler::endGameEndpoint(const std::shared_ptr<HttpRequest> &request) {
+  if (verifyPlayer(request)) {
+    std::string player = extractCookieValue(request);
+    auto response = HttpResponse::GETResponse(R"(true)");
+
+    gameHandler.reset();
+    isGuestConnected = false;
+
+    if (player.compare("playerX")) {
+      response.removeCookie("player", "playerX");
+    } else if (player.compare("playerO")) {
+      response.removeCookie("player", "playerO");
+    }
+
+    return response;
+  } else {
+    return HttpResponse::ERRORResponse("401", "UNAUTHORIZED");
+  }
+}
+
+HttpResponse
+HttpHandler::makeMoveEndpoint(const std::shared_ptr<HttpRequest> &request) {
+  if (verifyPlayer(request) && gameHandler) {
+    std::string player = extractCookieValue(request);
+    std::string boardId = (request->queryParams)["boardId"];
+    std::string segmentId = (request->queryParams)["segmentId"];
+
+    unsigned int boardIdIndex = std::stoul(boardId);
+    unsigned int segmentIdIndex = std::stoul(segmentId);
+
+    Point outerCoordinates =
+        BoardIndexConverter::IndexToPoint(boardIdIndex, boardSize);
+    Point innerCoordinates =
+        BoardIndexConverter::IndexToPoint(segmentIdIndex, boardSize);
+    std::array<Point, 2> move = {outerCoordinates, innerCoordinates};
+
+    bool isValid =
+        gameHandler->PerformMoveValidation(outerCoordinates, innerCoordinates);
+    if (isValid) {
+      gameHandler->PerformTurn(outerCoordinates, innerCoordinates);
+    }
+    return HttpResponse::GETResponse(
+        gameHandler->MoveAsJson(false, move, isValid));
+  } else {
+    return HttpResponse::ERRORResponse("401", "UNAUTHORIZED");
+  }
+}
+
+HttpResponse
+HttpHandler::createGameEndpoint(const std::shared_ptr<HttpRequest> &request) {
+  if (verifyPlayer(request)) {
+    return HttpResponse::GETResponse(R"(false)");
+  } else {
+    std::string playerType;
+    std::string gameMode = (request->queryParams)["gameMode"];
+    std::string playerSign = (request->queryParams)["playerSign"];
+    std::string boardSizeQuery = (request->queryParams)["boardSize"];
+    char playerSignAsChar = playerSign.c_str()[0];
+
+    if (playerSignAsChar != 'X' && playerSignAsChar != 'O') {
+      return HttpResponse::ERRORResponse("400", "Invalid player "
+                                                "figure while creating game.");
+    }
+
+    isPlayerVsComputer = gameMode == "Player_vs_AI" ? true : false;
+
+    boardSize = std::stoi(boardSizeQuery);
+    gameHandler = std::make_unique<GameHandler>(
+        std::stoi(boardSizeQuery), playerSignAsChar, isPlayerVsComputer);
+
+    // Assume game has been successfully created
+    auto response = HttpResponse::GETResponse(R"(true)");
+
+    if (playerSign == "X") {
+      hostSign = 'X';
+      guestSign = 'O';
+      if (!isPlayerVsComputer) {
+        response.setCookie("player", "playerX");
+      }
+    } else {
+      hostSign = 'O';
+      guestSign = 'X';
+      if (!isPlayerVsComputer) {
+        response.setCookie("player", "playerO");
+      }
+    }
+
+    if (isPlayerVsComputer) {
+      response.setCookie("player", "playerSolo");
+    }
+
+    return response;
+  }
+}
+
+HttpResponse
+HttpHandler::joinGameEndpoint(const std::shared_ptr<HttpRequest> &request) {
+  auto falseResponse = HttpResponse::GETResponse(R"(false)");
+  if (verifyPlayer(request) || gameHandler.get() == NULL) {
+  } else if (gameHandler.get() != NULL && isPlayerVsComputer) {
+  } else if (gameHandler.get() != NULL && !isPlayerVsComputer) {
+    if (isGuestConnected) {
+    } else {
+      isGuestConnected = true;
+      if (guestSign == 'X') {
+        auto response = HttpResponse::GETResponse(R"("X")");
+        response.setCookie("player", "playerX");
+        return response;
+      } else {
+        auto response = HttpResponse::GETResponse(R"("O")");
+        response.setCookie("player", "playerO");
+        return response;
+      }
+    }
+  }
+  return falseResponse;
+}
+
+HttpResponse HttpHandler::currentSegmentEndpoint() const {
+  auto segment = gameHandler->GetSegmentIndex();
+  return HttpResponse::GETResponse(std::to_string(segment));
 }
